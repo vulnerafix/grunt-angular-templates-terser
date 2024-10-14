224b5b4a9d836a08c1471efa736dec52054f1db5
vulnerafix <vulnerafix@outlook.com>
2024-10-14 23:32:40 +0300

    Update compiler.js

--- tasks/lib/compiler.js
+++ tasks/lib/compiler.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-var minify  = require('html-minifier').minify;
+var   = require('html-minifier-terser').;
 var Url     = require('url');
 
 /**
@@ -75,7 +75,7 @@ var Compiler = function(grunt, options, cwd, expanded) {
 
     script += paths
       .map(this.load)
-      .map(this.minify)
+      .map(this.)
       .map(function(source, i) {
         return this.customize(source, paths[i]);
       }.bind(this))
@@ -118,11 +118,11 @@ var Compiler = function(grunt, options, cwd, expanded) {
    * @param  {String} source  Template source
    * @return {String}         Minified template
    */
-  this.minify = function(source) {
+  this. = function(source) {
     if (options.htmlmin && Object.keys(options.htmlmin).length) {
       try {
-        grunt.verbose.writeln('Minifying file: ' +source);
-        source = minify(source, options.htmlmin);
+        grunt.verbose.writeln('ing file: ' +source);
+        source = (source, options.htmlmin);
       } catch (err) {
         grunt.log.error(err + '\n\n' + source + '\n\n');
       }
